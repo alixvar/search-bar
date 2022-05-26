87a12ad4a4cb0f7838039591a6527b465179e29f
Ali Sadeghy <alixvar68@gmail.com>
2022-05-26 17:43:07 +0430

    responsive

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,7 @@ import Search from "@mui/icons-material/Search";
 import Close from "@mui/icons-material/Close";
 import { useState } from "react";
 import "./SearchBar.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function SearchBar({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([]);
@@ -28,12 +29,12 @@ function SearchBar({ placeholder, data }) {
 
   return (
     <div className="flex flex-col bg-slate-300 h-screen items-center ">
-      <div>
-        <div className="mt-10 flex bg-white h-fit w-[400px]">
+      <div className="w-[75%] max-w-[400px] lg:max-w-[400px] ">
+        <div className="mt-16 flex bg-white h-fit">
           <input
             type="text"
             placeholder={placeholder}
-            className="outline-none flex-1 h-8 p-5 rounded-sm shadow-md py-6"
+            className="outline-none flex-1 rounded-sm shadow-md py-3 px-5"
             onChange={handleFilter}
             value={wordEntered}
           />
@@ -46,7 +47,7 @@ function SearchBar({ placeholder, data }) {
           </div>
         </div>
         {filteredData.length !== 0 && (
-          <div className="dataResault bg-white w-[340px] overflow-hidden overflow-y-auto h-fit max-h-[200px] shadow-md mt-2 transition-all">
+          <div className="dataResault bg-white max-w-[340px] overflow-hidden overflow-y-auto h-fit max-h-[200px] shadow-md mt-2 transition-all">
             {filteredData.map((value, key) => {
               return (
                 <a href={value.link} target="_blank" rel="noreferrer" key={key}>
@@ -58,6 +59,15 @@ function SearchBar({ placeholder, data }) {
             })}
           </div>
         )}
+      </div>
+      <div className="flex items-center p-2 border ">
+        <a
+          href="https://github.com/alixvar/search-bar"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GitHubIcon />
+        </a>
       </div>
     </div>
   );
